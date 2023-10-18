60b431990bdaad15c6602951e100379acd5803f2
Shubham Maurya <shubham.heeralal@gmail.com>
2023-10-18 13:09:35 +0530

    Added Schedule section

--- main.js
+++ main.js
@@ -39,3 +39,42 @@ modalButtons.forEach(modalButton => {
         document.body.addEventListener('click', hideModal)
     })
 })
+const players = [
+    "Hattie",
+    "Winston",
+    "Lena",
+    "Jaxton",
+    "Madilynn",
+    "Emmanuel",
+    "Alaya",
+    "Donald",
+    "Avalynn",
+    "Khari",
+    "Kora",
+]
+const scheduleData = []
+for (let i = 0; i < players.length / 2; i++) {
+    scheduleData.push({
+        player1: players[i],
+        player1Id: Math.floor(100000 + Math.random() * 900000),
+        player2: players[i + 1],
+        player2Id: Math.floor(200000 + Math.random() * 900000),
+    })
+}
+const garbaScheduleTable = document.querySelector('.garba-container .table')
+console.log(garbaScheduleTable)
+const scheduleTableItemTemplate = document.querySelector('#table-desc')
+scheduleData.forEach(schedule => {
+    const scheduleTableItem = scheduleTableItemTemplate.content.cloneNode(true);
+    const player1Name = scheduleTableItem.querySelector('.player-1-name')
+    const player1ID = scheduleTableItem.querySelector('.player-1-id')
+    const player2Name = scheduleTableItem.querySelector('.player-2-name')
+    const player2ID = scheduleTableItem.querySelector('.player-2-id')
+
+    player1Name.innerText = schedule.player1
+    player1ID.innerText = "ID " + schedule.player1Id
+    player2Name.innerText = schedule.player2
+    player2ID.innerText = "ID " + schedule.player2Id
+    
+    garbaScheduleTable.appendChild(scheduleTableItem)
+})
